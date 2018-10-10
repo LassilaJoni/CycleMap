8dd5de5b8446997e35ae1eba92d60ab36de029c1
LassilaJoni <joni@jonilassila.com>
2018-10-10 12:02:30 +0300

    Clean up code.

--- CycleMap/ViewController.swift
+++ CycleMap/ViewController.swift
@@ -11,123 +11,141 @@ import MapKit
 import CoreLocation
 import StoreKit
 
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate {
-    
-    @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var locationButton: MKUserTrackingBarButtonItem!
-    @IBOutlet weak var speedLabel: UILabel!
-    
-    let locationManager = CLLocationManager()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = NSLocalizedString("Cycling Maps", comment: "View Controller's title")
-        
-        addCycleMap()
-        setupLocationManager()
-        begForReviews()
-        
-        let searchController = UISearchController(searchResultsController: nil)
-        if #available(iOS 11.0, *) {
-            navigationItem.searchController = searchController
-            searchController.searchBar.delegate = self
-        }
-    }
-
-    
-    func addCycleMap() {
-        let overlay = MKTileOverlay(urlTemplate: "https://tile.thunderforest.com/cycle/{z}/{x}/{y}@2x.png?apikey=7760545f91504c6591737b342641aae3")
-        overlay.canReplaceMapContent = true
-        overlay.tileSize = CGSize(width: 512, height: 512)
-        mapView.addOverlay(overlay)
-    }
-    
-    func setupLocationManager() {
-        self.locationManager.delegate = self
-        self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.startUpdatingLocation()
-        self.locationButton.mapView = mapView
-    }
-    
-    func begForReviews() {
-        let appLaunchCount = UserDefaults.standard.integer(forKey: "AppLaunchCount")
-        let askedForReview = UserDefaults.standard.bool(forKey: "AskedForReview")
-        
-        if appLaunchCount >= 5 && !askedForReview {
-            if #available(iOS 10.3, *) {
-                SKStoreReviewController.requestReview()
-                UserDefaults.standard.set(true, forKey: "AskedForReview")
-            }
-        }
-        
-        UserDefaults.standard.set(appLaunchCount+1, forKey: "AppLaunchCount")
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.first else {
-            return
-        }
-        var speedKmh = location.speed * 3.6
-        var speedMph = location.speed / 0.44704
-        
-        if speedKmh < 0 || speedMph < 0 {
-            speedKmh = 0
-            speedMph = 0
-        }
-        
-        if Locale.current.usesMetricSystem {
-            self.speedLabel.text = "\(Int(speedKmh)) km/h"
-            
-        } else {
-            self.speedLabel.text = "\(Int(speedMph)) mph"
-        }
-        
-    }
-    
-    
-    
-    
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        if overlay is MKTileOverlay {
-            return MKTileOverlayRenderer(overlay: overlay)
-        }
-        
-        return MKOverlayRenderer()
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        self.navigationItem.searchController?.resignFirstResponder()
-        self.navigationItem.searchController?.dismiss(animated: true, completion: nil)
-        
-        let searchRequest = MKLocalSearch.Request()
-        searchRequest.naturalLanguageQuery = searchBar.text
-        
-        MKLocalSearch(request: searchRequest).start { (response, error) in
-            if let error = error {
-                print(error)
-                return
-            }
-            guard let response = response else {
-                return
-            }
-            
-            let annotations = self.mapView.annotations
-            self.mapView.removeAnnotations(annotations)
-            
-            let latitude = response.boundingRegion.center.latitude
-            let longitude = response.boundingRegion.center.longitude
-            
-            let annotation = MKPointAnnotation()
-            annotation.title = searchBar.text
-            annotation.subtitle = response.mapItems.first?.name
-            annotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
-            self.mapView.addAnnotation(annotation)
-            
-            let span = MKCoordinateSpan.init(latitudeDelta: 0.06, longitudeDelta: 0.06)
-            let region = MKCoordinateRegion.init(center: annotation.coordinate, span: span)
-            self.mapView.setRegion(region, animated: true)
-            
-        }
-    }
+class ViewController: UIViewController {
+	
+	// MARK Outlets
+	
+	@IBOutlet private var mapView: MKMapView!
+	@IBOutlet private var locationButton: MKUserTrackingBarButtonItem!
+	@IBOutlet private var speedLabel: UILabel!
+	
+	// MARK: Stored Properties
+	
+	private lazy var locationManager = CLLocationManager()
+	
+	// MARK: Lifecycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = NSLocalizedString("Cycling Maps", comment: "View Controller's title")
+		
+		addOverlay()
+		setupLocationManager()
+		checkRateStatus()
+		
+		let searchController = UISearchController(searchResultsController: nil)
+		
+		if #available(iOS 11.0, *) {
+			navigationItem.searchController = searchController
+			searchController.searchBar.delegate = self
+		}
+	}
+	
+	// MARK: Private Methods
+	
+	private func addOverlay() {
+		let apiKey = "7760545f91504c6591737b342641aae3"
+		
+		let overlay = MKTileOverlay(urlTemplate: String(format: "https://tile.thunderforest.com/cycle/{z}/{x}/{y}@2x.png?apikey=%@", apiKey))
+		
+		overlay.canReplaceMapContent = true
+		overlay.tileSize = CGSize(width: 512, height: 512)
+		
+		mapView.addOverlay(overlay)
+	}
+	
+	private  func setupLocationManager() {
+		locationManager.delegate = self
+		locationManager.requestWhenInUseAuthorization()
+		locationManager.startUpdatingLocation()
+		locationButton.mapView = mapView
+	}
+	
+	private func checkRateStatus() {
+		let appLaunchCount = UserDefaults.standard.integer(forKey: "AppLaunchCount")
+		let askedForReview = UserDefaults.standard.bool(forKey: "AskedForReview")
+		
+		if appLaunchCount >= 5 && !askedForReview {
+			if #available(iOS 10.3, *) {
+				SKStoreReviewController.requestReview()
+				UserDefaults.standard.set(true, forKey: "AskedForReview")
+			}
+		}
+		
+		UserDefaults.standard.set(appLaunchCount+1, forKey: "AppLaunchCount")
+	}
 }
 
+// MARK: MKMapViewDelegate
+
+extension ViewController: MKMapViewDelegate {
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		return MKTileOverlayRenderer(overlay: overlay)
+	}
+}
+
+// MARK: CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		guard let location = locations.first else {
+			return
+		}
+		
+		var speedKmh = location.speed * 3.6
+		var speedMph = location.speed / 0.44704
+		
+		if speedKmh < 0 || speedMph < 0 {
+			speedKmh = 0
+			speedMph = 0
+		}
+		
+		if Locale.current.usesMetricSystem {
+			speedLabel.text = "\(Int(speedKmh)) km/h"
+			
+		} else {
+			speedLabel.text = "\(Int(speedMph)) mph"
+		}
+		
+	}
+}
+
+// MARK: UISearchBarDelegate
+
+extension ViewController: UISearchBarDelegate {
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		navigationItem.searchController?.resignFirstResponder()
+		navigationItem.searchController?.dismiss(animated: true, completion: nil)
+		
+		let searchRequest = MKLocalSearch.Request()
+		searchRequest.naturalLanguageQuery = searchBar.text
+		
+		MKLocalSearch(request: searchRequest).start { (response, error) in
+			if let error = error {
+				print(error)
+				return
+			}
+			
+			guard let response = response else {
+				return
+			}
+			
+			let annotations = self.mapView.annotations
+			self.mapView.removeAnnotations(annotations)
+			
+			let latitude = response.boundingRegion.center.latitude
+			let longitude = response.boundingRegion.center.longitude
+			
+			let annotation = MKPointAnnotation()
+			annotation.title = searchBar.text
+			annotation.subtitle = response.mapItems.first?.name
+			annotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+			self.mapView.addAnnotation(annotation)
+			
+			let span = MKCoordinateSpan.init(latitudeDelta: 0.06, longitudeDelta: 0.06)
+			let region = MKCoordinateRegion.init(center: annotation.coordinate, span: span)
+			self.mapView.setRegion(region, animated: true)
+			
+		}
+	}
+}
